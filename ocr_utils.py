6a8762b07ee091023106d1db32808b9507ced5a6
Gitae-Kwon <kitae0601@gmail.com>
2025-08-07 09:59:08 +0900

    Update ocr_utils.py

--- ocr_utils.py
+++ ocr_utils.py
@@ -1,16 +1,18 @@
-# ocr_utils.py
 import requests
 
 def extract_text_from_ocr_space(image_bytes, api_key):
     url = 'https://api.ocr.space/parse/image'
     payload = {
+        'apikey': api_key,
         'isOverlayRequired': False,
-        'apikey': K89682508288957,
-        'language': 'kor'  # 한국어
+        'language': 'kor'
     }
-    files = {'filename': image_bytes}
+    files = {'filename': ('image.jpg', image_bytes)}
+
     response = requests.post(url, data=payload, files=files)
     result = response.json()
+
     if result.get("IsErroredOnProcessing"):
         raise ValueError(result.get("ErrorMessage", ["Unknown error"])[0])
+
     return result['ParsedResults'][0]['ParsedText']
